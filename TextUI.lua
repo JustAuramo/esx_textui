18e2844030377e26bbd9c84429912ba7373bba37
Auramo <108193124+JustAuramo@users.noreply.github.com>
2023-11-30 11:36:22 +0200

    Add files via upload

---
+++ TextUI.lua
@@ -0,0 +1,45 @@
+Debug = ESX.GetConfig().EnableDebug
+local isShowing = false
+---@param message string
+---@param typ string
+local function TextUI(message, typ)
+    isShowing = true
+    lib.showTextUI(message){
+        position = "left-center",
+        icon = icon or '',
+        style = {
+            borderRadius= 5,
+            backgroundColor = '#0985e3f8',
+            color = 'white'
+        }
+    }
+end
+
+local function HideUI()
+   if not isShowing then return end
+   isShowing = false
+    lib.hideTextUI(message)
+end
+
+exports('TextUI', TextUI)
+exports('HideUI', HideUI)
+RegisterNetEvent('ESX:TextUI', TextUI)
+RegisterNetEvent('ESX:HideUI', HideUI)
+
+if Debug then
+    RegisterCommand("textui:error", function()
+        ESX.TextUI("i ~r~love~s~ donuts", "error")
+    end)
+
+    RegisterCommand("textui:success", function()
+        ESX.TextUI("i ~g~love~s~ donuts", "success")
+    end)
+
+    RegisterCommand("textui:info", function()
+        ESX.TextUI("i ~b~love~s~ donuts", "info")
+    end)
+
+    RegisterCommand("textui:hide", function()
+        ESX.HideUI()
+    end)
+end
